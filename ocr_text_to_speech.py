3c7756fdd221128011b3932497ae4822adca7cf0
Afaq Ahmad <afaq.ahmad100@gmail.com>
2019-07-25 15:55:04 +0500

    Add files via upload

---
+++ ocr_text_to_speech.py
@@ -0,0 +1,143 @@
+import sys
+from os import path
+
+import cv2
+import numpy as np
+
+from PyQt5 import QtCore
+from PyQt5 import QtWidgets
+from PyQt5 import QtGui
+
+import pytesseract
+from PIL import Image
+from pytesseract import image_to_string
+from gtts import gTTS
+import os
+
+#pytesseract.pytesseract.TesseractNotFoundError: tesseract is not installed or it's not in your path
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+#tessdata_dir_config = r'--tessdata-dir "<replace_with_your_tessdata_dir_path>"'
+tessdata_dir_config = r'--tessdata-dir "C:\Program Files\Tesseract-OCR\tessdata"'
+
+
+
+class RecordVideo(QtCore.QObject):
+    image_data = QtCore.pyqtSignal(np.ndarray)
+
+    def __init__(self, camera_port=0, parent=None):
+        super().__init__(parent)
+        self.camera = cv2.VideoCapture(camera_port)
+
+        self.timer = QtCore.QBasicTimer()
+
+    def start_recording(self):
+        self.timer.start(0, self)
+
+    
+    def timerEvent(self, event):
+        if (event.timerId() != self.timer.timerId()):
+            return
+
+        read, data = self.camera.read()
+        if read:
+            self.image_data.emit(data)
+    def framesave(self):
+        
+        read, data = self.camera.read()
+        if read:
+            cv2.imwrite('a.png',data)
+            img=Image.fromarray(data)
+            img.load()
+            
+            text=pytesseract.image_to_string(img, lang='spa', config=tessdata_dir_config)
+            print ('Text_Found: ',text,len(text))
+            if len(text)>0:
+                tts = gTTS(text=text, lang='ES')# for english language use (lang='en')
+                tts.save("pcvoice.mp3")
+                os.system("start pcvoice.mp3")
+
+
+class FaceDetectionWidget(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.image = QtGui.QImage()
+        self._red = (0, 0, 255)
+        self._width = 2
+        self._min_size = (30, 30)
+
+
+    def image_data_slot(self, image_data):
+
+
+        
+        self.image = self.get_qimage(image_data)
+        if self.image.size() != self.size():
+            self.setFixedSize(self.image.size())
+
+        self.update()
+    
+        
+        
+    def get_qimage(self, image: np.ndarray):
+        height, width, colors = image.shape
+        bytesPerLine = 3 * width
+        QImage = QtGui.QImage
+
+        image = QImage(image.data,
+                       width,
+                       height,
+                       bytesPerLine,
+                       QImage.Format_RGB888)
+
+        image = image.rgbSwapped()
+        return image
+
+    def paintEvent(self, event):
+        painter = QtGui.QPainter(self)
+        painter.drawImage(0, 0, self.image)
+        self.image = QtGui.QImage()
+
+
+class MainWidget(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        
+        self.face_detection_widget = FaceDetectionWidget()
+
+        # TODO: set video port
+        self.record_video = RecordVideo()
+
+        image_data_slot = self.face_detection_widget.image_data_slot
+        self.record_video.image_data.connect(image_data_slot)
+
+        layout = QtWidgets.QVBoxLayout()
+
+        layout.addWidget(self.face_detection_widget)
+        self.run_button = QtWidgets.QPushButton('Start')
+        layout.addWidget(self.run_button)
+
+        self.run_button.clicked.connect(self.record_video.start_recording)
+
+        self.screenshot = QtWidgets.QPushButton('Snap Shot')
+        layout.addWidget(self.screenshot)
+
+        self.screenshot.clicked.connect(self.record_video.framesave)
+        self.setLayout(layout)
+
+
+    
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+
+    main_window = QtWidgets.QMainWindow()
+    main_widget = MainWidget()
+    main_window.setCentralWidget(main_widget)
+    main_window.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+
+    main()
